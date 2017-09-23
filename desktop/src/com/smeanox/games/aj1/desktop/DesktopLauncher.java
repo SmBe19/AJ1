80699aa471dfc9a68d15509861c81f215e8f8d06
Benjamin Schmid <git@schmidb.ch>
2017-09-23 10:36:54 +0200

    initial commit

---
+++ desktop/src/com/smeanox/games/aj1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smeanox.games.aj1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smeanox.games.aj1.AJ1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AJ1(), config);
+	}
+}
